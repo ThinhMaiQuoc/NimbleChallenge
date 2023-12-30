29789b62c10d61b7381b62d4c19a54a037c2b1e1
ThinhMaiQuoc <thinh.quocc@gmail.com>
2023-12-31 00:55:34 +0700

    search report screen

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/pages/Dashboard';
+import SearchReport from './components/pages/SearchReport';
 import withAuth from './HOC/withAuth';
 
 const AuthenticatedDashboard = withAuth(Dashboard);
+const AuthenticatedReport = withAuth(SearchReport);
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<AuthenticatedDashboard />} />
+          <Route path="/search-report" element={<AuthenticatedReport />} />
         </Routes>
       </div>
     </Router>
